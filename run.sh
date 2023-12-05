757d0a165f4a4adfe166dcbb76214cebed6fc930
Piers Barber <kochab.96@gmail.com>
2023-12-05 09:27:21 +0000

    Added zip

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-echo "I am running successfully"
-echo "Yippee!"
-echo "Look at my webhook"
-echo "My webhook's amazing"
+echo "I am running successfully" > 1.txt
+echo "Yippee!" > 2.txt
+echo "Look at my webhook" > 3.txt
+echo "My webhook's amazing" > 4.txt
+
+zip archive.zip *.txt
+
